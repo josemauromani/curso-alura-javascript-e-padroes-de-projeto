2fbefa7a6ed57bcf96d1e363a94d99f8065f6f84
dermoanamnese <josemauromani@gmail.com>
2020-05-19 18:32:53 -0300

    negociacao controller

---
+++ client/js/app/controllers/NegociacaoController.js
@@ -0,0 +1,16 @@
+class NegociacaoController {
+
+    constructor() {
+        let $ = document.querySelector.bind(document);
+        this._inputData = $('#data')
+        this._inputQuantidade = $('#quantidade')
+        this._inputValor = $('#valor')
+    }
+
+    adiciona(event) {
+        event.preventDefault()
+        let data = new Date(this._inputData.value.split('-'))
+        console.log(data)
+    }
+
+}
