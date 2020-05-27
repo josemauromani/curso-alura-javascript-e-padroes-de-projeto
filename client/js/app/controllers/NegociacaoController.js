d0235c06936b3da34e681ee8a4faedcbb8302e31
dermoanamnese <josemauromani@gmail.com>
2020-05-27 17:54:02 -0300

    Model e View Negociacao

--- client/js/app/controllers/NegociacaoController.js
+++ client/js/app/controllers/NegociacaoController.js
@@ -6,17 +6,15 @@ class NegociacaoController {
         this._inputQuantidade = $('#quantidade')
         this._inputValor = $('#valor')
         this._listaNegociacoes = new ListaNegociacoes();
+        this._negociacoesView = new NegociacoesView($('#negociacoesViewId'));
+        this._negociacoesView.update(this._listaNegociacoes)
     }
 
     adiciona(event) {
         event.preventDefault()
         this._listaNegociacoes.adiciona(this._criaNegociacao())
-
-        this._listaNegociacoes.negociacoes.push(this._criaNegociacao())
-
+        this._negociacoesView.update(this._listaNegociacoes)
         this._limpaFormulario()
-
-        console.log(this._listaNegociacoes.negociacoes)
     }
 
     _criaNegociacao() {
