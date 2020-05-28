c9ba08fad693b69e046efbd82286f2dc0a2228fd
dermoanamnese <josemauromani@gmail.com>
2020-05-28 12:38:44 -0300

    Classe Mensagem

--- client/js/app/controllers/NegociacaoController.js
+++ client/js/app/controllers/NegociacaoController.js
@@ -1,19 +1,29 @@
 class NegociacaoController {
 
     constructor() {
-        let $ = document.querySelector.bind(document);
+        let $ = document.querySelector.bind(document)
         this._inputData = $('#data')
         this._inputQuantidade = $('#quantidade')
         this._inputValor = $('#valor')
-        this._listaNegociacoes = new ListaNegociacoes();
-        this._negociacoesView = new NegociacoesView($('#negociacoesViewId'));
+
+        this._listaNegociacoes = new ListaNegociacoes()
+        this._negociacoesView = new NegociacoesView($('#negociacoesViewId'))
         this._negociacoesView.update(this._listaNegociacoes)
+        
+        this._msg = new Mensagem()
+        this._mensagemView = new MensagemView($('#mensagemViewID'))
+        this._mensagemView.update(this._msg)
     }
 
     adiciona(event) {
         event.preventDefault()
+        
         this._listaNegociacoes.adiciona(this._criaNegociacao())
         this._negociacoesView.update(this._listaNegociacoes)
+
+        this._msg.texto='Negociação adicionada com sucesso'
+        this._mensagemView.update(this._msg)        
+
         this._limpaFormulario()
     }
 
